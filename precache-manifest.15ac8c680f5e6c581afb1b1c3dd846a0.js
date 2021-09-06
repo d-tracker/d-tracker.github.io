89bfd1d8010ac7f0bd8b57bf6ffc79bba3871832
scarger <supremeterminal@gmail.com>
2021-09-06 19:45:36 -0400

    changed server endpoint

---
+++ precache-manifest.15ac8c680f5e6c581afb1b1c3dd846a0.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f346d5bd692627ee26a39e4766922b83",
+    "url": "/auth/success.html"
+  },
+  {
+    "revision": "abba89f8e373bbc170b4",
+    "url": "/css/chunk-vendors.896e5f23.css"
+  },
+  {
+    "revision": "0dfafb8c1e77b718f354",
+    "url": "/css/settings.9357cc44.css"
+  },
+  {
+    "revision": "e35a7cd7e3f1accc1830",
+    "url": "/css/stats.8ab79986.css"
+  },
+  {
+    "revision": "aa90f63368e7dceab30467b684f782a9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e291a360a13bf916b961",
+    "url": "/js/app.0bcac5ef.js"
+  },
+  {
+    "revision": "abba89f8e373bbc170b4",
+    "url": "/js/chunk-vendors.ee4c6d73.js"
+  },
+  {
+    "revision": "9b5c1e4c1b4464ebb71a",
+    "url": "/js/login.178ffdcc.js"
+  },
+  {
+    "revision": "0dfafb8c1e77b718f354",
+    "url": "/js/settings.49061f5b.js"
+  },
+  {
+    "revision": "e35a7cd7e3f1accc1830",
+    "url": "/js/stats.7f8e151d.js"
+  },
+  {
+    "revision": "6fb79685f89dc8ece5264be574330af4",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
